030a3364c873b50e8536243fa7b2ad411baf57a3
rahulthakur2001 <rahulthakur95609@gmail.com>
2025-03-05 18:20:33 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+function App() {
+
+  return (
+    <>
+      <h1>Vite + React</h1>
+      <h1 className="bg-red-500 text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    </>
+  )
+}
+
+export default App
