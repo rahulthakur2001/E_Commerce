1b4700e5cd6070a3bfea78ed801333e8528e6701
Raj05052002 <thrajkumar9@gmail.com>
2025-03-06 16:20:24 +0530

    raj

--- src/Layouts/Homelayout.jsx
+++ src/Layouts/Homelayout.jsx
@@ -1,15 +1,14 @@
 import React from 'react'
 import { Header } from '../Components/Header/Header'
 import { Outlet } from 'react-router-dom'
+import { Footer } from '../Footer/Footer'
 
 export const Homelayout = () => {
   return (
     <div>
         <Header/>
         <Outlet/>
-        <div className="pt-4">
-            <h1>Footer Home Layout</h1>
-        </div>
+        <Footer/>
     </div>
   )
 }
